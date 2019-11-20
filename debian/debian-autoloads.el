f5b4f6b328797153b74d031f2fcede0d27c77d13
Nicholas D Steeves <nsteeves@gmail.com>
2019-11-19 20:03:04 -0500

    Set elpy-rpc-virtualenv-path to "'global"…

--- debian/debian-autoloads.el
+++ debian/debian-autoloads.el
@@ -2,4 +2,5 @@
 (setq elpy-rpc-pythonpath nil
       python-shell-interpreter "/usr/bin/python3"
       python-shell-interpreter-args "-i"
-      elpy-rpc-python-command "/usr/bin/python3")
+      elpy-rpc-python-command "/usr/bin/python3"
+      elpy-rpc-virtualenv-path 'global)
